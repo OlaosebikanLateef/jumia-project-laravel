5b6d5224ad443ec58b65979cb163377cca2edb1c
OlaosebikanLateef <laolaosebikan@gmail.com>
2023-04-20 10:09:38 +0100

    new update

--- database/migrations/2023_04_20_080715_create_countries_table.php
+++ database/migrations/2023_04_20_080715_create_countries_table.php
@@ -11,13 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('countries', function (Blueprint $table) {
             $table->id();
-            $table->string('firstname');
-            $table->string('lastname');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('confirm_password');
+            $table->string('country_name');
             $table->timestamps();
         });
     }
@@ -27,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('countries');
     }
 };
